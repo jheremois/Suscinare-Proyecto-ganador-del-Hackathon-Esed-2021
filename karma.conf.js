5985fadb2ee2eae047dad2c4a1bf57ffcd333071
jheremy <jheremy808@gmail.com>
2021-08-17 20:43:06 -0400

    ready dash

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/zhefi'),
+      dir: require('path').join(__dirname, './coverage/suscinare'),
       subdir: '.',
       reporters: [
         { type: 'html' },
