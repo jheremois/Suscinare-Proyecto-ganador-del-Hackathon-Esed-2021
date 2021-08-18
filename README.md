41b8cffe3261ba543ab12332d86dd68a42ea1d32
jheremy <jheremy808@gmail.com>
2021-08-17 21:10:54 -0400

    heroku test

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# zhefi
+# suscinare
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'zhefi'`, () => {
+  it(`should have as title 'suscinare'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('zhefi');
+    expect(app.title).toEqual('suscinare');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('zhefi app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('suscinare app is running!');
   });
 });
