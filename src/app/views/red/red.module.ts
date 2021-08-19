1230cfe85a6b21a8b9af28f791bd057f18f4f2a3
Yefri Coplin <jcwhite1199@gmail.com>
2021-08-19 01:37:52 -0400

    fix: module import in red-module

--- src/app/views/red/red.module.ts
+++ src/app/views/red/red.module.ts
@@ -1,7 +1,9 @@
-import { MaterialModule } from '@shared/material/material.module';
 import { ComponentsModule } from '@shared/components/components.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 
 import { RedRoutingModule } from './red-routing.module';
 import { MainComponent } from './pages/main/main.component';
@@ -15,7 +17,8 @@ import { MainComponent } from './pages/main/main.component';
     CommonModule,
     RedRoutingModule,
     ComponentsModule,
-    MaterialModule
+    MatChipsModule,
+    MatIconModule
   ]
 })
 export class RedModule { }
